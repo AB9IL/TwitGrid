f493434eb50dda55b1e40f9542adf95b18276b01
Philip Colllier <redcrown01@gmail.com>
2021-11-21 02:18:35 +0000

    Update runtwitgrid.sh
    Removed hardcoded topics.
    Switched to a single file containing all topics and handles.
    Manual handle entry works correctly in either fzf or Rofi.

--- runtwitgrid.sh
+++ runtwitgrid.sh
@@ -8,80 +8,77 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 # This script makes it easy to have a curated list of Twitter profiles to
-# read on several topics.  You need TwitGrid and you need to keep several
-# small topic files with a few Twitter handles in them (one handle per line).
+# read on several topics.  You need TwitGrid and you need to keep one
+# topic file with a few topics and Twitter handles.
 
-# The syntax for topics is:
-# one per line
-# separate the topic name from the topic file with a comma
-# Example:
-# Linux,tw_linux
+# The syntax for topic file is:
+# topic name, handle1 handle2 handle3 handle4 handle5
+#
+# Notes:
+# no comments
+# one topic per line
+# separate the topic name from the Twitter hanhdles with a comma
+# one space between Twitter handles
+#
+# Examples:
+# Linux Experts, manjarolinux nixcraft itsfoss2 linux4everyone omgubuntu
+# Open Source Intelligence, bellingcat auroraintel michaeldweiss Gerjon_ arictoler
 
 #==============================================================================
 # User defined variables
 # path to TwitGrid directory
 twitpath="/usr/local/sbin/TwitGrid"
 
-# path to topic files
-topicpath=$twitpath
-
-# List of topics
-OPTIONS="Thinkers,tw_thinkers
-Open Source Intelligence 0,tw_osint-1
-Open Source Intelligence 1,tw_osint-2
-Infosec,tw_infosec
-Linux,tw_linux
-CPU and GPU Technology,tw_cpugputech
-Oil and Gas Industries,tw_oilgas
-ADS-B Monitors,tw_adsb
-USA Politics,tw_uspolitics
-Renewable Energy,tw_renenergy
-Radio Technology,tw_rftech
-Software Defined Radio,tw_rfsdr
-Shortwave Radio,tw_shortwave
-Popular Traders,tw_traders
-China Watch,tw_cnwatch
-User Defined List,Userdefined"
+# path to topic file
+topicpath="$twitpath/tw_alltopics"
 
 # Browser command
 BROWSER="x-www-browser --new-tab"
 
 #==============================================================================
+# CAUTION: DRAGONS LIVE BELOW THIS LINE 
+#==============================================================================
 getlist(){
 # Create a group of user-defined Twitter profiles.
-PROFILE_LIST="$(rofi -dmenu -p "Enter one to five Twitter handles" )"
+[[ "$OPMODE" == "gui" ]] && PROFILE_LIST="$(rofi -dmenu -p "Enter one to five Twitter handles")"
+[[ "$OPMODE" != "gui" ]] && echo "Enter one to five Twitter handles" && read PROFILE_LIST
+
 # Exit if no data is entered.
 [[ -z "$PROFILE_LIST" ]] && echo "something went wrong" && exit 1
-LISTSTRING=$PROFILE_LIST
+
+PROFILES=$PROFILE_LIST
 }
 
+# define operating mode
+OPMODE=$1
+
+# read the bookmarks
+readarray OPTIONS < $topicpath
+
 # open a menu in Rofi or fzf
-[[ "$1" == "gui" ]] && COMMAND="rofi -dmenu -p Select"
-[[ "$1" == "gui" ]] || COMMAND="fzf --layout=reverse --header=Select:"
+[[ "$OPMODE" == "gui" ]] && COMMAND="rofi -dmenu -p Select -lines ${#OPTIONS[@]}"
+[[ "$OPMODE" == "gui" ]] || COMMAND="fzf --layout=reverse --header=Select:"
 
 # Select the desired topic
-REPLY=$(echo "${OPTIONS[@]}" | sed '/^$/d' | awk -F, '{printf $1"\n"}' | $COMMAND )
+REPLY=$(echo "${OPTIONS[@]}" | sed 's/^ //g;/^$/d' | awk -F, '{printf $1"\n"}' | $COMMAND )
 
 # Quit if no selection
 [[ -z "$REPLY" ]] && echo "No selection made, exiting..." && exit 0
 
 # Match the topic and list of profiles
-PROFILES=$(echo "${OPTIONS[@]}" | grep "$REPLY" | awk -F, '{printf $2"\n"}')
+PROFILES=$(echo "${OPTIONS[@]}" | grep "$REPLY" | sed 's/^ //g;s/, /,/g' | awk -F, '{printf $2"\n"}')
 
 # Quit if unable to get profiles
 [[ -z "$PROFILES" ]] && echo "Something went wrong, exiting..." && exit 1
 
-[[ "$PROFILES" == "Userdefined" ]] || readarray -t LIST < $topicpath/$PROFILES
+[[ "$PROFILES" == "Userdefined" ]] || LIST=$PROFILES
 [[ "$PROFILES" == "Userdefined" ]] && getlist
 
 echo "
-Reading handles from: "$PROFILES
-
-# Convert LIST array string LISTSTRING if taking string from user input.
-[[ "${#LIST[@]}" -gt "1" ]] && LISTSTRING=$(IFS=" "; echo "${LIST[*]}")
+Reading handles: $PROFILES"
 
 # Write the profile list into the html file
-sed -i "s/const handlesTopInterests.*/const handlesTopInterests = \'$LISTSTRING\';/g" \
+sed -i "s/const handlesTopInterests.*/const handlesTopInterests = \'$PROFILES\';/g" \
     $twitpath/twitgrid.html
 
 #Start TwitGrid
